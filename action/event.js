78c379b7a4e43992fabf7cb34e168b8d875d6590
Humphrey47 <humphreymbise68@gmail.com>
2025-08-11 18:17:31 +0300

    Create event.js

---
+++ action/event.js
@@ -0,0 +1,50 @@
+const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE'; 
+const botname = process.env.BOTNAME || 'EMP';
+
+const Events = async (client, Nick) => {
+    
+    try {
+        let metadata = await client.groupMetadata(Nick.id);
+        let participants = Nick.participants;
+        let desc = metadata.desc || "No Description";
+        let groupMembersCount = metadata.participants.length;
+
+        for (let num of participants) {
+            let dpuser;
+
+            try {
+                dpuser = await client.profilePictureUrl(num, "image");
+            } catch {
+                dpuser = "https://files.catbox.moe/s5nuh3.jpg";
+            }
+
+            if (Nick.action === "add") {
+                let userName = num;
+
+                let Welcometext = `@${userName.split("@")[0]} Holla👋,\n\nWelcome to ${metadata.subject}.\n\nYou might want to read group description,\nFollow group rules to avoid being removed.\n\n ${botname} 2025.`;
+                if (welcomegoodbye === 'TRUE') {
+                    await client.sendMessage(Nick.id, {
+                        image: { url: dpuser },
+                        caption: Welcometext,
+                        mentions: [num],
+                        });
+                }
+            } else if (Nick.action === "remove") {
+                let userName2 = num;
+
+                let Lefttext = `@${userName2.split("@")[0]} Goodbye we shall miss you😔.\n\nAnyway Goodbye .`;
+                if (welcomegoodbye === 'TRUE') {
+                    await client.sendMessage(Nick.id, {
+                        image: { url: dpuser },
+                        caption: Lefttext,
+                        mentions: [num],
+                    });
+                }
+               }
+              }
+             } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = Events;
